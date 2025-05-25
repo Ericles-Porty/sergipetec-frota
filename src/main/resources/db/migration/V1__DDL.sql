e4f747c012d68331f5c98da5d5b895d0e889fdfa
Éricles Dos Santos <67772327+Ericles-Porty@users.noreply.github.com>
2025-05-25 18:14:10 -0300

    refactor: update SQL scripts for consistent casing and data types in vehicle tables

--- src/main/resources/db/migration/V1__DDL.sql
+++ src/main/resources/db/migration/V1__DDL.sql
@@ -1,16 +1,16 @@
 CREATE TABLE Veiculo (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     modelo VARCHAR(100) NOT NULL,
     fabricante VARCHAR(100) NOT NULL,
     ano INT NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
-    tipo VARCHAR(10) NOT NULL CHECK (tipo IN ('carro', 'moto'))
+    tipo VARCHAR(10) NOT NULL CHECK (tipo IN ('CARRO', 'MOTO'))
 );
 
 CREATE TABLE Carro (
     veiculo_id INT PRIMARY KEY,
     quantidade_portas INT NOT NULL,
-    tipo_combustivel VARCHAR(10) NOT NULL CHECK (tipo_combustivel IN ('gasolina', 'etanol', 'diesel', 'flex')),
+    tipo_combustivel VARCHAR(10) NOT NULL CHECK (tipo_combustivel IN ('GASOLINA', 'ETANOL', 'DIESEL', 'FLEX')),
     FOREIGN KEY (veiculo_id) REFERENCES Veiculo(id) ON DELETE CASCADE
 );
 
