36595a56e8d09b2a1e242b3d1bfbd0a57ee398f5
Éricles Dos Santos <67772327+Ericles-Porty@users.noreply.github.com>
2025-05-25 22:33:51 -0300

    refactor: standardize formatting and improve readability in GlobalExceptionHandler

--- src/main/java/xyz/xpto/frota/api/configurations/GlobalExceptionHandler.java
+++ src/main/java/xyz/xpto/frota/api/configurations/GlobalExceptionHandler.java
@@ -20,93 +20,92 @@ import xyz.xpto.frota.api.extensions.StandardResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    /**
-     * Trata exceções de validação customizadas.
-     */
-    @ExceptionHandler(CustomValidationException.class)
-    public ResponseEntity<StandardResponse<Void>> handleCustomValidationException(CustomValidationException ex) {
-        return StandardResponse.badRequest(ex.getErrors());
-    }
+	/**
+	 * Trata exceções de validação customizadas.
+	 */
+	@ExceptionHandler(CustomValidationException.class)
+	public ResponseEntity<StandardResponse<Void>> handleCustomValidationException(CustomValidationException ex) {
+		return StandardResponse.badRequest(ex.getErrors());
+	}
 
-    /**
-     * Trata exceções de validação do Bean Valid.
-     */
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<StandardResponse<Void>> handleValidationException(ValidationException ex) {
-        return StandardResponse.badRequest(List.of(ex.getMessage()));
-    }
+	/**
+	 * Trata exceções de validação do Bean Valid.
+	 */
+	@ExceptionHandler(ValidationException.class)
+	public ResponseEntity<StandardResponse<Void>> handleValidationException(ValidationException ex) {
+		return StandardResponse.badRequest(List.of(ex.getMessage()));
+	}
 
-    /**
-     * Trata exceções de validação de constraints.
-     */
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<StandardResponse<Void>> handleConstraintViolationException(ConstraintViolationException ex) {
-        List<String> errors = ex.getConstraintViolations().stream()
-                .map(violation -> violation.getMessage())
-                .collect(Collectors.toList());
-        return StandardResponse.badRequest(errors);
-    }
+	/**
+	 * Trata exceções de validação de constraints.
+	 */
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<StandardResponse<Void>> handleConstraintViolationException(ConstraintViolationException ex) {
+		List<String> errors = ex.getConstraintViolations().stream()
+				.map(violation -> violation.getMessage())
+				.collect(Collectors.toList());
+		return StandardResponse.badRequest(errors);
+	}
 
-    /**
-     * Trata exceções de validação de argumentos de métodos.
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        List<String> errors = ex.getBindingResult().getAllErrors().stream()
-                .map(error -> error.getDefaultMessage())
-                .collect(Collectors.toList());
+	/**
+	 * Trata exceções de validação de argumentos de métodos.
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<List<String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+		List<String> errors = ex.getBindingResult().getAllErrors().stream()
+				.map(error -> error.getDefaultMessage())
+				.collect(Collectors.toList());
 
-        return ResponseEntity.badRequest().body(errors);
-    }
+		return ResponseEntity.badRequest().body(errors);
+	}
 
-    /**
-     * Trata exceções de status de resposta.
-     */
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<StandardResponse<Void>> handleResponseStatusException(
-            ResponseStatusException ex) {
-        return ResponseEntity.status(ex.getStatusCode())
-                .body(new StandardResponse<>(false, ex.getStatusCode().value(), null,
-                        "Houve um erro ao processar a requisição.",
-                        List.of(ex.getReason())));
-    }
+	/**
+	 * Trata exceções de status de resposta.
+	 */
+	@ExceptionHandler(ResponseStatusException.class)
+	public ResponseEntity<StandardResponse<Void>> handleResponseStatusException(
+			ResponseStatusException ex) {
+		return ResponseEntity.status(ex.getStatusCode())
+				.body(new StandardResponse<>(false, ex.getStatusCode().value(), null,
+						"Houve um erro ao processar a requisição.",
+						List.of(ex.getReason())));
+	}
 
-    /**
-     * Trata exceções de leitura de JSON inválido.
-     */
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<StandardResponse<Void>> handleHttpMessageNotReadableException(
-            HttpMessageNotReadableException ex) {
-        return StandardResponse.badRequest(List.of("Verifique se o corpo da requisição tem um JSON válido."));
-    }
+	/**
+	 * Trata exceções de leitura de JSON inválido.
+	 */
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<StandardResponse<Void>> handleHttpMessageNotReadableException(
+			HttpMessageNotReadableException ex) {
+		return StandardResponse.badRequest(List.of("Verifique se o corpo da requisição tem um JSON válido."));
+	}
 
-    /**
-     * Trata exceções de tipo de mídia não suportado.
-     */
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<StandardResponse<Void>> handleHttpMediaTypeNotSupportedException(
-            HttpMediaTypeNotSupportedException ex) {
-        return StandardResponse
-                .badRequest(List.of("Verifique se o cabeçalho Content-Type da requisição está correto."));
-    }
+	/**
+	 * Trata exceções de tipo de mídia não suportado.
+	 */
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	public ResponseEntity<StandardResponse<Void>> handleHttpMediaTypeNotSupportedException(
+			HttpMediaTypeNotSupportedException ex) {
+		return StandardResponse
+				.badRequest(List.of("Verifique se o cabeçalho Content-Type da requisição está correto."));
+	}
 
-    /**
-     * Trata exceções de acesso inválido ao banco de dados.
-     */
-    @ExceptionHandler(InvalidDataAccessResourceUsageException.class)
-    public ResponseEntity<StandardResponse<Void>> handleInvalidDataAccessResourceUsageException(
-            InvalidDataAccessResourceUsageException ex) {
-                System.out.println(ex);
-        return StandardResponse.error("Houve um erro ao acessar o banco de dados.");
-    }
+	/**
+	 * Trata exceções de acesso inválido ao banco de dados.
+	 */
+	@ExceptionHandler(InvalidDataAccessResourceUsageException.class)
+	public ResponseEntity<StandardResponse<Void>> handleInvalidDataAccessResourceUsageException(
+			InvalidDataAccessResourceUsageException ex) {
+		System.out.println(ex);
+		return StandardResponse.error("Houve um erro ao acessar o banco de dados.");
+	}
 
-    // TODO: Adicionar tratamento de exceções gerais ao final do desenvolvimento.
-    /**
-     * Trata exceções genéricas.
-     */
-    // @ExceptionHandler(Exception.class)
-    // public ResponseEntity<StandardResponse<Void>> handleException(Exception ex) {
-    // return StandardResponse.error("Houve um erro inesperado ao processar a
-    // requisição.");
-    // }
+	// TODO: Adicionar tratamento de exceções gerais ao final do desenvolvimento.
+	/**
+	 * Trata exceções genéricas.
+	 */
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<StandardResponse<Void>> handleException(Exception ex) {
+		return StandardResponse.error("Houve um erro inesperado ao processar a requisição.");
+	}
 }
