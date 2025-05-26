1689feca11ac039e9084a528250bf156422fe8c9
Éricles Dos Santos <67772327+Ericles-Porty@users.noreply.github.com>
2025-05-26 14:21:35 -0300

    fix: update footer copyright to reflect correct names

--- frontend/components/AppFooter.vue
+++ frontend/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
 	<footer class="w-full p-5 flex flex-row justify-center items-center">
-		<p>&copy; 2025 Minha Aplicação</p>
+		<p>&copy; 2025 Ericles & Sergipetec</p>
 	</footer>
 </template>
